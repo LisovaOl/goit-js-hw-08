93700eee544370fa1eff4479f22bce0552f69eec
LisovaOl <lisova.olenka@gmail.com>
2022-08-06 17:22:24 +0300

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
 
+// import { save, getItemKey } from './storage';
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+
+// const VIDEOPLAYER_CURRENT_TIME = 'videoplayer-current-time';
+
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+
+// player.on('timeupdate', throttle(assaaes, 1000));
+
+// function assaaes({ seconds }) {
+//   save(VIDEOPLAYER_CURRENT_TIME, seconds);
+// }
+
+// if (getItemKey(VIDEOPLAYER_CURRENT_TIME)) {
+//   player.setCurrentTime(getItemKey(VIDEOPLAYER_CURRENT_TIME));
+// }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+// import { save, getItemKey, removeKey } from './storage.js';
+// import throttle from 'lodash.throttle';
+// const FEEDBACK_FORM_STATE = 'feedback-form-state';
+// let formData = getItemKey(FEEDBACK_FORM_STATE) || {};
+
+// const form = document.querySelector('.feedback-form');
+
+// form.addEventListener('input', throttle(saveValue, 500));
+
+// function saveValue(e) {
+//   formData[e.target.name] = e.target.value;
+//   save(FEEDBACK_FORM_STATE, JSON.stringify(formData));
+// }
+
+// // localStorage
+
+// localStorageValue();
+
+// function localStorageValue() {
+//   const proverka = getItemKey(FEEDBACK_FORM_STATE);
+//   if (proverka) {
+//     if (proverka.email) {
+//       form.email.value = proverka.email;
+//     }
+//     if (proverka.message) {
+//       form.message.value = proverka.message;
+//     }
+//   }
+// }
+
+// // SABMIT;
+
+// form.addEventListener('submit', handleSubmit);
+
+// function handleSubmit(e) {
+//   e.preventDefault();
+//   const {
+//     elements: { email, message },
+//   } = e.currentTarget;
+
+//   if (email.value === '' || message.value === '') {
+//     return alert('Please fill in all the fields!');
+//   }
+//   removeKey(FEEDBACK_FORM_STATE);
+//   const formData = new FormData(form);
+//   const valuesFotm = Object.fromEntries(formData.entries());
+//   e.currentTarget.reset();
+// }
 
