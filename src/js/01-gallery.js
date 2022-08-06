504329c276e01bbdabb8e98ecf7877152f78f541
LisovaOl <lisova.olenka@gmail.com>
2022-08-06 17:08:04 +0300

    npm install simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,46 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const imgMarkup = createImgMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", imgMarkup);
+
+function createImgMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  <a class = "gallery__item" href = "${original}">
+    <img
+      class = "gallery__image"
+      src = "${preview}"
+      alt = "${description}"
+    />
+  </a>`;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionPosition: "bottom",
+  captionsData: "alt",
+  captionDelay: 250,
+  enableKeyboard: true,
+  // spinner: true,
+  // navText: ['prev', 'next'],
+  close: false,
+  // alertError: true,
+  // alertErrorMessage: "Зображення не знайдено, буде завантажено наступне зображення",
+  fadeSpeed: 300,
+});
+
+// lightbox.on("show.simplelightbox", function (e) {
+
+//     console.log(e.target);
+
+// });
